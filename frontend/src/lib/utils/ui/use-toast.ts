f82a8ee7d4325082f09e48ef5b3ddb57163c3a13
Bruno Matos Silva <bmsdevfullstack@gmail.com>
2024-04-11 11:57:00 -0300

    fix: import datepicker and toast

--- frontend/src/lib/utils/ui/use-toast.ts
+++ frontend/src/lib/utils/ui/use-toast.ts
@@ -2,11 +2,8 @@
 
 // Inspired by react-hot-toast library
 import * as React from "react"
+import { ToastActionElement, ToastProps } from "./toast";
 
-import type {
-  ToastActionElement,
-  ToastProps,
-} from "src/lib/utils/ui/toast"
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
--- frontend/src/view/components/DatePickerInput.tsx
+++ frontend/src/view/components/DatePickerInput.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps, useState } from "react";
+import { useState } from "react";
 import { cn } from "../../lib/utils";
 import { CrossCircledIcon } from "@radix-ui/react-icons";
 import { formatDate } from "../../app/utils/formatDate";
