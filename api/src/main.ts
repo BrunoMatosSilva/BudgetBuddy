100fe014ecc70943fe1476c19171549a2dd96b8f
Bruno Matos Silva <bmsdevfullstack@gmail.com>
2024-04-12 11:20:07 -0300

    fix: enable origin

--- api/src/main.ts
+++ api/src/main.ts
@@ -9,11 +9,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors({
-    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Request-With', 'Accept'],
-    exposedHeaders: ['Authorization'],
-    origin: ['http://localhost:5173', 'https://budgetbuddy-x2wz.onrender.com/'],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    origin: '*'
   })
 
   await app.listen(3000);
