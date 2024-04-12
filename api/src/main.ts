1131b161af7f496e7df61b8a1e5da3d911bbe91f
Bruno Matos Silva <bmsdevfullstack@gmail.com>
2024-04-12 09:47:57 -0300

    fix: enableCors

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,7 +8,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe())
-  app.enableCors()
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
 
   await app.listen(3000);
 }
